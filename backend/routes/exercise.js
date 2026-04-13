23c21155b719bfa5a64e4dc19d02d905cf98f0d7
Raquel-Larios <raquel.larios.susin@alumnos.upm.es>
2026-04-14 00:01:15 +0200

    [Update] Archivo no subido del push anterior

--- backend/routes/exercise.js
+++ backend/routes/exercise.js
@@ -6,16 +6,23 @@ const categoryController = require("../controllers/categoryController");
 const videoController = require("../controllers/videoController");
 
 //GESTIÓN EJERCICIO
-router.get("/all", (req, res) => {
-    db.query('SELECT * FROM ejercicio ORDER BY nombre_ejercicio ASC', (err, results) => {
+router.get("/all/nombre", (req, res) => {
+    db.query('SELECT id, nombre_ejercicio, id_categoria FROM ejercicio ORDER BY nombre_ejercicio ASC', (err, results) => {
         if (err) throw err;
         res.json(results);
     });
 });
 
-router.get("/:ejercicioId", (req, res) => {
-    const ejercicioId = parseInt(req.params.ejercicioId, 10);
-    db.query('SELECT * FROM ejercicio WHERE id = ?', [ejercicioId], (err, results) => {
+router.get("/all/by-categoria", (req, res) => {
+    db.query('SELECT ejercicio.id, nombre_ejercicio, ejercicio.id_categoria, nombre_categoria FROM ejercicio LEFT JOIN categoria ON ejercicio.id_categoria = categoria.id ORDER BY nombre_categoria IS NULL, nombre_categoria, nombre_ejercicio ASC', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+router.get("/:id_ejercicio", (req, res) => {
+    const id_ejercicio = parseInt(req.params.id_ejercicio, 10);
+    db.query('SELECT id, nombre_ejercicio, id_categoria FROM ejercicio WHERE id = ?', [id_ejercicio], (err, results) => {
         if (err) throw err;
         res.json(results);
     })
